1398869a3da99f7bd7a6c33db9c21a11cb4cdebb
Jiub255 <boycemj255@gmail.com>
2025-05-15 17:37:11 -0700

    added multiple heads to cheat class to account for multiple of the same key in a row

--- misc/cheats/cheat.gd
+++ misc/cheats/cheat.gd
@@ -2,28 +2,35 @@ class_name Cheat
 extends Resource
 # TODO: Add timeout. Run process from whatever instantiates checker, through each of its cheats.
 
-signal activated(effect : Enums.CheatEffect)
 
+signal activated(effect : Enums.CheatEffect)
 
 @export var _effect : Enums.CheatEffect
 @export var _code : Array[Key]
-var _head : int
+var _heads : Array[int]
 
 
 func check(key : Key):
-	print(f'head: {_head}, cheat key: {_code[_head]}, input key: {key}')
-	if _code[_head] == key:
-		_advance_head()
-	else:
-		_reset_head()
+	# Start a new head at 0 each check.
+	_heads.append(0)
+	# Iterating backwards because of potential array deletions.
+	for i in range(len(_heads) - 1, -1, -1):
+		var head = _heads[i]
+		#print(f'head: {head}, cheat key: {_code[head]}, input key: {key}')
+		if _code[head] == key:
+			_advance_head(i)
+		else:
+			_reset_head(i)
+	#print(f'head count: {len(_heads)}')
 
 
-func _advance_head():
-	_head += 1
-	if _head == len(_code):
-		_reset_head()
+func _advance_head(head_index : int):
+	_heads[head_index] += 1
+	if _heads[head_index] == len(_code):
+		_reset_head(head_index)
 		activated.emit(_effect)
+		#print(f'Cheat activated: {_effect}')
 
 
-func _reset_head():
-	_head = 0
+func _reset_head(head_index : int):
+	_heads.remove_at(head_index)
